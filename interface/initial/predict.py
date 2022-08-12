e9a7b9754a6f17efbbe3b7c3b0b58c29abb59c4a
PaulaGonzalez01 <paulagonzalez011499@gmail.com>
2022-08-12 11:34:52 -0500

    Interface code

---
+++ interface/initial/predict.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras.preprocessing import image
+import numpy as np
+from keras.applications.vgg16 import preprocess_input
+
+
+def predict_one(fruit, model, img_data):
+
+    model_h5 = keras.models.load_model(f'./models/{fruit}_{model}.h5')
+    img = image.image_utils.load_img(img_data, target_size=(256, 256))
+    img_array = keras.preprocessing.image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    classes = model_h5.predict(preprocess_input(img_array))
+    if classes[0][0] > 0.5:
+        return "FRESH"
+    elif classes[0][1] > 0.5:
+        return "HAS DEFECT"
